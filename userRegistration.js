accbe02c2d7b6f82399a31e11744239b1e2ad2cc
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-20 00:19:25 +0530

    Refactored: to user choice

--- userRegistration.js
+++ userRegistration.js
@@ -1,11 +1,28 @@
 let utility = require("./userRegistrationutility.js");
 var readlineSync = require('readline-sync');
-test = function () {
-	utility.validateFirstName();
- 	utility.validateLastName();
- 	utility.validateEmail();
- 	utility.validateMobileNumber();
- 	utility.validatePassword();
-}
-test();
+// taking user input// 
+choiceUserRegistration = function () {
+	let isTerminated = false;
+	while (isTerminated == false) {
+		let option = readlineSync.question(`Enter your choice to check:\n\n 1.FirstName\n 2.LastName\n 3.Email\n 4.PhoneNumber\n 5.password\n 6.Exit\n`)
+		switch (option) {
+			case 1: utility.validateFirstName();
+				break;
+			case 2: utility.validateLastName();
+				break;
+			case 3: utility.validateEmail();
+				break;
+			case 4: utility.validateMobileNumber();
+				break;
+			case 5: utility.validatePassword();
+				break;
+			case 6: isTerminated = true;
+				break;
+			default:
+				console.log("Wrong Input");
+				// option.close();
+		}
+	}
 
+}
+choiceUserRegistration();
