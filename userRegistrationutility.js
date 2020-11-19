7465e2e317468cd5b5eba016938d4d76856a243e
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-20 00:09:29 +0530

    Added: validate email method

--- userRegistrationutility.js
+++ userRegistrationutility.js
@@ -42,6 +42,27 @@ class Registration {
 			console.log(e);
 		}
 	}
+	//to validate email
+	validateEmail = () => {
+		try {
+			const read = /^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$/;
+			let text = readlineSync.question(`Enter Valid Email Address:`);
+			// let result = text.match(read);
+			const result = read.exec(text);
+			if (result) {
+				console.log("Valid");
+			}
+			else {
+				console.log("Invalid");
+			}
+			// alert(result);
+			// const result = match.matches();
+			//console.log(result);
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}
 
 
 }
