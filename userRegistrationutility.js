6d13b845446fa9dd54c149f60e532d2bd44713e0
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-20 00:06:23 +0530

    Added: last name method

--- userRegistrationutility.js
+++ userRegistrationutility.js
@@ -21,7 +21,28 @@ class Registration {
 			console.log(e);
 		}
 	}
-	
+	//to validate last name//
+	validateLastName = () => {
+		try {
+			const read = /[A-Z][a-z]{3,}/;
+			let text = readlineSync.question(`Enter Valid Last Name:`);
+			// let result = text.match(read);
+			const result = read.exec(text);
+			if (result) {
+				console.log("Valid");
+			}
+			else {
+				console.log("Invalid");
+			}
+			// alert(result);
+			// const result = match.matches();
+			//console.log(result);
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}
+
 
 }
 module.exports = new Registration();
